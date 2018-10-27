4ccca57be83c5bcfac2944ea7db61da8e87f1d66
Pierre-Marie Dartus <p.dartus@salesforce.com>
2018-10-27 10:14:21 -0700

    Simple echo server

--- experiments/simple-http/src/main.rs
+++ experiments/simple-http/src/main.rs
@@ -1,5 +1,5 @@
 use std::io::prelude::*;
-use std::io::BufReader;
+use std::io::{BufReader, BufWriter};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
@@ -24,15 +24,30 @@ fn run() {
     }
 }
 
-fn handle_connection(stream: TcpStream) {
-    let mut reader = BufReader::new(stream);
-    
+#[derive(Debug)]
+struct Request {
+    method: String,
+    uri: String,
+    http_version: String,
+    headers: Vec<Header>,
+}
+
+#[derive(Debug)]
+struct Header {
+    name: String,
+    content: String,
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
+
     let mut request_line = String::new();
+
     reader.read_line(&mut request_line);
+    let (method, uri, http_version) = parse_request_line(&request_line);
 
-    println!("Request line: {}", request_line);
-
-    let mut headers: Vec<String> = vec![];
+    let mut headers: Vec<Header> = vec![];
     loop {
         let mut next_line = String::new();
         reader.read_line(&mut next_line);
@@ -40,16 +55,53 @@ fn handle_connection(stream: TcpStream) {
         if next_line == "\r\n" {
             break;
         } else {
-            headers.push(next_line);
+            let header = parse_header(&next_line);
+
+            headers.push(header);
         }
     }
 
-    println!("headers:\n{:#?}", headers);
+    let request = Request {
+        method,
+        uri,
+        http_version,
+        headers,
+    };
+
+    println!("Incoming request:\n{:#?}", request);
+
+    let body = format!("echo: {}", request.uri);
+    let headers = [
+        "HTTP/1.1 200 OK", 
+        &format!("Cotent-Length: {}", body.bytes().len()),
+        "\r\n"
+    ].join("\r\n").to_string();
+    let response = headers + &body;
+
+    println!("Response:\n{:#?}", response);
+    
+    writer.write(&response.into_bytes());
 }
 
-// fn read_next_line(&mut reader: BufReader<TcpStream>) -> String {
-//     let mut line = String::new();
-//     reader.read_line(&mut line);
+fn parse_request_line(line: &String) -> (String, String, String) {
+    let items: Vec<&str> = line.split(' ').collect();
 
-//     line.pop().pop()
-// }
+    (
+        items[0].to_owned(),
+        items[1].to_owned(),
+        // Remove the "\r\n"
+        items[2][..(items[2].len() - 2)].to_owned(),
+    )
+}
+
+// TODO: Add better error handling
+fn parse_header(line: &String) -> Header {
+    let column_index = line.find(':').unwrap();
+    let (name, content) = line.split_at(column_index);
+
+    Header {
+        name: name.to_owned(),
+        // Remove the ": " and "\r\n" prefixing the content
+        content: content[2..(content.len() - 2)].to_owned(),
+    }
+}
