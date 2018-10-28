563f607d6e974e0043f46641468e07d812b8b18c
Pierre-Marie Dartus <p.dartus@salesforce.com>
2018-10-28 07:40:02 -0700

    Proper command line

--- experiments/simple-http/src/lib.rs
+++ experiments/simple-http/src/lib.rs
@@ -1,13 +1,21 @@
+use std::error::Error;
 use std::io::prelude::*;
 use std::io::{BufReader, BufWriter};
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream, SocketAddr};
 
 mod request;
 
-pub fn run() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap_or_else(|e| {
-        panic!("Something went wrong when binding the socket: {}", e);
-    });
+#[derive(Debug)]
+pub struct Config {
+    pub port: u16,
+    pub path: String,
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], config.port));
+    let listener = TcpListener::bind(addr)?;
+
+    println!("Server running at: http://{}", addr);
 
     for incoming in listener.incoming() {
         match incoming {
@@ -16,10 +24,13 @@ pub fn run() {
                 handle_connection(stream);
             }
             Err(e) => {
+                // TODO: Improve error handling
                 panic!("Something went wrong in the connection: {}", e);
             }
         }
     }
+
+    Ok(())
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -27,18 +38,20 @@ fn handle_connection(mut stream: TcpStream) {
     let mut writer = BufWriter::new(&stream);
 
     let request = request::Request::read(&mut reader).unwrap();
-    
+
     println!("Incoming request:\n{:#?}", request);
 
     let body = format!("echo: {}", request.uri);
     let headers = [
-        "HTTP/1.1 200 OK", 
+        "HTTP/1.1 200 OK",
         &format!("Cotent-Length: {}", body.bytes().len()),
-        "\r\n"
-    ].join("\r\n").to_string();
+        "\r\n",
+    ]
+        .join("\r\n")
+        .to_string();
     let response = headers + &body;
 
     println!("Response:\n{:#?}", response);
-    
+
     writer.write(&response.into_bytes());
 }
--- experiments/simple-http/src/main.rs
+++ experiments/simple-http/src/main.rs
@@ -1,5 +1,45 @@
+extern crate clap;
 extern crate simple_http;
 
+use clap::{App, Arg, ArgMatches};
+use simple_http::{run, Config};
+use std::process;
+
 fn main() {
-    simple_http::run();
+    let matches = App::new("simple-http")
+        .version("0.1.0")
+        .author("Pierre-Marie Dartus <dartus.pierremarie@gmail.com>")
+        .about("Simplistic experimental HTTP server")
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .help("Port to run the server on")
+                .default_value("8080"),
+        ).arg(
+            Arg::with_name("path")
+                .index(1)
+                .help("Path to serve")
+                .default_value("."),
+        ).get_matches();
+
+    let config = parse_cli_args(matches);
+
+    if let Err(e) = run(config) {
+        eprintln!("{}", e);
+        process::exit(1);
+    };
+}
+
+fn parse_cli_args(matches: ArgMatches) -> Config {
+    let port_str = matches.value_of("port").unwrap();
+
+    let port = port_str.parse::<u16>().unwrap_or_else(|_| {
+        eprintln!("Invalid port parameter: {}", port_str);
+        process::exit(1);
+    });
+
+    let path = matches.value_of("path").unwrap().to_owned();
+
+    Config { port, path }
 }
